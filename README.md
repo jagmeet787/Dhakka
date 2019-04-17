64a4b898a948f0ed651f39565aa06459d8057d0f
Jagmeet Singh <jagmeet787@gmail.com>
2019-04-17 21:49:14 +0530

    file upload

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+**Falcon**
 ```
 pip install falcon
+pip install falcon-multipart
 pip install waitress
 waitress-serve --port=8000 main:app
+```
+
+**Tools**
+maldrolyzer <br>
+https://github.com/maldroid/maldrolyzer <br>
+Installation <br>
+```
+pip install -U androguard
+pip install pycrypto
+pip install pyelftools
+pip install yara
 ```
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import falcon
+from falcon_multipart.middleware import MultipartMiddleware
+import io
+import os
+import shutil
 
 class MainResource(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.body = ('\nHttp Response')
 
-app = falcon.API()
+    def on_post(self, req, resp):
+    	apk = req.get_param('apk')
+    	raw = apk.file.read()
+    	if apk.filename:
+    		filename = apk.filename
+    		open(filename, 'wb').write(raw)
+    	resp.status = falcon.HTTP_201
+
+api = application = app = falcon.API(middleware=[MultipartMiddleware()])
 
 main = MainResource()
 
