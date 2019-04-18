08c210d8efa0d6cb726feaed75f31967366c3e2c
Jagmeet Singh <jagmeet787@gmail.com>
2019-04-18 23:55:17 +0530

    get all records service

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import falcon
 from falcon_multipart.middleware import MultipartMiddleware
 # from falcon_cors import CORS 
 from upload import UploadResource
-
+from database import database
+import json
 # cors = CORS(allow_origins_list=['*://*/*']) 
 api = application = app = falcon.API(middleware=[MultipartMiddleware()])
-
+database_service = database()
 # public_cors = CORS(allow_all_origins=True)
 
 
@@ -40,12 +41,19 @@ class ImageResource(object):
         with open(filename, 'r') as f:
             resp.body = f.read()
 
+class RecordsResource(object):
+    def on_get(self, req, resp):
+        all_records = database_service.all_records()
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps(all_records)
+
 upload_resource = UploadResource()
 # t_res = ThingsResource()
 static_resource = StaticResource()
 image_resource = ImageResource()
-
+all_records_resource = RecordsResource()
 app.add_route('/upload', upload_resource)
+app.add_route('/allrecords', all_records_resource)
 # app.add_route('/test', t_res)
 app.add_route('/{filename}', static_resource)
 app.add_route('/image/{filename}', image_resource)
